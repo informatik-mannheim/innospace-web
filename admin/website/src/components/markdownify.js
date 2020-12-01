d04c5135fca5008594e6ecb06c468c5633bb503c
ma-walter <67912186+ma-walter@users.noreply.github.com>
2020-12-01 12:19:01 +0100

    webeditor deleted

--- admin/website/src/components/markdownify.js
+++
@@ -1,8 +0,0 @@
-import React, { Fragment } from 'react';
-import Markdown from 'react-markdown';
-
-const Markdownify = ({ source }) => (
-  <Markdown renderers={{ root: Fragment, paragraph: Fragment }}>{source}</Markdown>
-);
-
-export default Markdownify;
